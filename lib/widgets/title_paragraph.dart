5e1572401c940d95135ff67c3118157278055c53
yusuke kadowaki <yusuke.kadowaki.1231@gmail.com>
2020-08-14 22:22:39 +0900

    fix title paragraph padding

--- lib/widgets/title_paragraph.dart
+++ lib/widgets/title_paragraph.dart
@@ -7,55 +7,43 @@ class TitleParagraph extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      //FIXME:
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.red,
-          width: 8,
-        ),
-      ),
       //
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 0,
-          vertical: 10,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AutoSizeText(
-              '市川たべ蔵',
-              maxLines: 1,
-              textAlign: TextAlign.left,
-              style: GoogleFonts.sawarabiMincho(
-                textStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const AutoSizeText(
-              'Tabezo Ichikawa',
-              maxLines: 1,
-              textAlign: TextAlign.left,
-              style: TextStyle(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AutoSizeText(
+            '市川たべ蔵',
+            maxLines: 1,
+            textAlign: TextAlign.left,
+            style: GoogleFonts.sawarabiMincho(
+              textStyle: const TextStyle(
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
-                fontSize: 40,
-              ),
-            ),
-            const AutoSizeText(
-              'Music and video and ...something creator(s) currently based in Hamamatsu city, Japan.',
-              textAlign: TextAlign.left,
-              maxLines: 1,
-              style: TextStyle(
                 color: Colors.white,
                 fontSize: 20,
               ),
             ),
-          ],
-        ),
+          ),
+          const AutoSizeText(
+            'Tabezo Ichikawa',
+            maxLines: 1,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 40,
+            ),
+          ),
+          const AutoSizeText(
+            'Music and video and ...something creator(s) currently based in Hamamatsu city, Japan.',
+            textAlign: TextAlign.left,
+            maxLines: 2,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ],
       ),
     );
   }
