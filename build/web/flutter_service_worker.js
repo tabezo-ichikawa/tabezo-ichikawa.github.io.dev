8f4b0189bae3ea941aa731af587145d5ee45a6e5
yusuke kadowaki <yusuke.kadowaki.1231@gmail.com>
2020-08-23 22:43:09 +0900

    Delete the font shadow since it looked bad on iPhone

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "1695018a6ddefaed6c617bfa00019b3e",
 "/": "1695018a6ddefaed6c617bfa00019b3e",
-"main.dart.js": "bb7409e22c708e713c76cd707e967f22",
+"main.dart.js": "2c3f903671759b8b86cc8df75a681f76",
 "manifest.json": "4500a9ad4595f1bd54be76bc2d6e41e2"
 };
 
